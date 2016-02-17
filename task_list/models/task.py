3d39185b2dc7ccb6e6c7de7acd3ccf795332b001
Vilmar Schmelzer <vschmelzer@localhost.localdomain>
2016-02-17 13:49:38 -0200

    Correction models database

--- task_list/models/task.py
+++ task_list/models/task.py
@@ -4,7 +4,8 @@ from task_list import db
 class Task(db.Model):
 
     __tablename__ = 'task'
-
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
     task = db.Column(db.Text)
+
+    def __init__(self, task=None):
+        self.task = task
